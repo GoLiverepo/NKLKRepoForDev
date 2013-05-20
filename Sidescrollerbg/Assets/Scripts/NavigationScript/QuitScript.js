6d6ec9f9f64efa7ff2f64c396394b6238de58ac3
vinamra0991 <misra.vinamra@gmail.com>
2013-05-20 15:19:26 +0530

    updated with menu screens

---
+++ Sidescrollerbg/Assets/Scripts/NavigationScript/QuitScript.js
@@ -0,0 +1,24 @@
+#pragma strict
+
+function Start () {
+
+}
+
+function Update () 
+{
+	for (var i = 0; i < Input.touchCount; ++i) 
+	{ 
+	   	var touch:Touch = Input.touches[i];
+	    var ray:Ray = Camera.main.ScreenPointToRay(touch.position); 
+	    var hit:RaycastHit = new RaycastHit();
+	    if(Physics.Raycast(ray,hit, 100000)) 
+		{ 
+	    	if(hit.collider.tag == "Quit")
+	     	{
+	     		renderer.material.color = Color.red;
+	   			Application.Quit();
+	     	}
+		}		
+	}
+
+}
